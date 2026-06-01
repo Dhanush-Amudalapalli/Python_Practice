586a29a6a7577117045fb56f3e443022e590493f
Dhanush-Amudalapalli <153887121+Dhanush-Amudalapalli@users.noreply.github.com>
2026-06-01 23:33:07 +0530

    Add files via upload

---
+++ Task_2.py
@@ -0,0 +1,8 @@
+name=input("Enter your name: ")
+marks=int(input("Enter your marks: "))
+
+if(marks>=40):
+    print(name +" scored "+ str(marks) +" PASS ")
+
+else:
+    print(name +" scored "+ str(marks) +"FAIL")
