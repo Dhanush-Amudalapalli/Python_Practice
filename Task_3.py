156d053f134c2f9b9317b100b785725698f8187d
Dhanush-Amudalapalli <153887121+Dhanush-Amudalapalli@users.noreply.github.com>
2026-06-01 23:36:00 +0530

    Add files via upload

---
+++ Task_3.py
@@ -0,0 +1,8 @@
+num=int(input("enter the number: "))
+for i in range (1,num + 1):
+    print(i)
+
+
+num2=int(input("enter the number which is multiple of 2 : "))
+for i in range (2,num2 + 1, 2):
+    print(i)
